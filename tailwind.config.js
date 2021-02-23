0124be2279ac86860f1c7723efb7d39d6f67f6fe
Nguyen Tran Canh <46088035+CanhhnaC@users.noreply.github.com>
2021-02-23 15:14:40 +0700

    [fix] tailwin config (#3)
    Co-authored-by: Canh Nguyen Tran <canh.nguyen1@ntq-solutions.com.vn>

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     applyComplexClasses: true,
   },
   purge: {
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
     options: {
       safelist: {
         standard: ['outline-none'],
